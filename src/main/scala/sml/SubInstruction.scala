9967ab5960fe63f7a4b57417b3043ae98e36455f
GreatKarollo <karol.sudol@icloud.com>
2016-02-13 19:44:28 +0000

    SubInstruction

---
+++ src/main/scala/sml/SubInstruction.scala
@@ -0,0 +1,24 @@
+package sml
+
+/**
+  *   1. function obtains @params from registers: opt1 and opt2
+  *   2. performs SUB on  both params
+  *   3. writes result to register
+  */
+class SubInstruction (label: String, op: String, val result: Int, val op1: Int, val op2: Int) extends Instruction(label, op) {
+
+  override def execute(m: Machine) {
+    val value1 = m.regs(op1)
+    val value2 = m.regs(op2)
+    m.regs(result) = value1 - value2
+  }
+
+  override def toString(): String = {
+    super.toString + " " + op1 + " - " + op2 + " to " + result + "\n"
+  }
+}
+
+object SubInstruction {
+  def apply(label: String, result: Int, op1: Int, op2: Int) =
+    new SubInstruction(label, "sub", result, op1, op2)
+}
